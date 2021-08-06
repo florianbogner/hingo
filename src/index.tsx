81bc8491aec46dfe2d07003ddbdb41482984fcf4
florianbogner <florianbogner@me.com>
2021-08-06 16:52:12 +0200

    basic job card

--- src/index.tsx
+++ src/index.tsx
@@ -3,95 +3,159 @@ import ReactDOM from 'react-dom';
 import styled, { createGlobalStyle } from 'styled-components';
 
 import bg from './resources/bg-hingo.jpg'
+import logo from './resources/logo-hingo.svg'
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Lexend&display=swap');
 
   body {
-  margin: 0;
-  font-family: 'Lexend', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 14px;
+    margin: 0;
+    font-family: 'Lexend', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 14px;
     overflow: hidden;
-}
+  }
+
+  a:link{
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  a:visited{
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  a:hover{
+    color: rgba(255, 255, 255);
+  }
+
+  a:active{
+    color: rgba(255, 255, 255);
+  }
 `
+
 const Header = styled.div`
-  @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap');
-  font-family: 'Lexend', sans-serif;
-
-  font-size: 24px;
-  color: #ffffff;
-  text-shadow: 0px 0px 32px rgba(85, 0, 185, 1);
-  background-color: rgba(255, 255, 255, 0.3);;
-  border-bottom: 1px solid white;
-
+  box-sizing: border-box;
   width: 100%;
+  display: flex;
   justify-content: left;
   align-items: center;
-
   padding-left: 24px;
-  padding-top: 16px;
-  padding-bottom: 16px;
+  min-height: 64px;
+  background-color: rgba(255, 255, 255, 0.3);
 
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  z-index: 2;
+  font-size: 24px;
+  color: #ffffff;  
+  background-size: 100% 100%;
+  text-shadow: 0px 0px 32px rgba(255, 255, 255, 1);
+  border-bottom: 1px solid white;
+`
+
+const HeaderIcon= styled.img`
+  width: 36px;
+  pointer-events: none;
+  padding-right: 8px;
+
+  -webkit-filter: drop-shadow(0px 0px 32px rgba(255, 255, 255, 0.5));
+  filter: drop-shadow( 0px 0px 32px rgba(255, 255, 255, 0.5));
 `
 
 const Background = styled.div`
   height: 100vh;
   width: 100vw;
+
   background-image: url(${bg});
-  background-position: left top;
+  background-position: center top;
   background-repeat: no-repeat;
   background-size: cover;
-  opacity: 0.8;
-  -webkit-transform: scaleX(-1);
-  transform: scaleX(-1);
+`
 
-  filter: blur(100px);
-  -webkit-filter: blur(100px);
+const ContentContainer = styled.div`
+  display: grid;
+  place-items: center;
+`
+
+const Line = styled.hr`
+  display: block;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  border-width:0;
+  height: 1px;
+  background-color: #ebebeb;
 `
 
 const Card = styled.div`
-  z-index: 3;
-  font-size: 24px;
-  color: #000000;
+  margin-top: 64px;
+  max-width: 486px;
+  padding: 24px;
+
   background-color: #ffffff;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1), 0 4px 24px rgba(0, 0, 0, 0.1);
+  border-radius: 24px;
+`
+const CardHeader = styled.div`
+  font-size: 24px;
 `
 
-const Copyright = styled.div`
+const CardBody = styled.div`
+  color: #929292;
+  padding-top: 8px;
+`
+
+const History = styled.div`
+  margin-top: 64px;
+  max-width: 486px;
+  padding: 24px;
+
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 24px;
+`
+
+const Footer = styled.div`
   position: absolute;
   bottom: 16px;
   left: 16px;
-  z-index: 2;
-
-  font-size: 14px;
-  color: #ffffff;
 `
 
 function Title(){
   return (
     <Header>
+      <HeaderIcon src={logo} alt={'Hingo-Logo'}/>
       Hingo
     </Header>
   )
 }
 
+function CardContent(){
+  return(
+    <div>
+      <CardHeader>heuristic-name</CardHeader>
+      <Line />
+      <div>In unbekannten Situationen orientieren wir uns tendenziell am Verhalten anderer Personen, die uns ähnlich sind.</div>
+      <CardBody>Finde kreative Lösungen zur Darstellung sozialen Verhaltens, um ein Gefühl von Sicherheit zu erzeugen und Menschen zu einer Entscheidung zu bewegen. Das kann zum Beispiel in Form von Zahlen (Favorisiert von, Anzahl der Views, Kommentare, …), positiven Testimonials oder durch die Darstellung der Handlungen oder Ergebnisse der anderen Nutzer passieren.</CardBody>
+    </div>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <Title/>
-
-    <Background>
-      <Card>asdasd</Card>
-    </Background>
-
-    <Copyright>
-      © Florian Bogner 2021
-    </Copyright>
+    <React.StrictMode>
+      <GlobalStyle/>
+      <Background>
+        <Title/>
+        <ContentContainer>
+          <Card>
+            <CardContent/>
+          </Card>
+          <History>
+            Aktuelle Liste
+          </History>
+          <Footer>
+            <a href="https://www.linkedin.com/in/florian-bogner-84a907142/" target="_blank" >Florian Bogner 2021</a>
+          </Footer>
+        </ContentContainer>
+      </Background>
+    </React.StrictMode>
 
   
   </React.StrictMode>,
